be0d8bf572bde741f65f4e097f093f065afffba5
nabin-8 <nabinacharya740@gmail.com>
2025-02-16 02:00:32 +0545

    artists

--- django_prc/artists/models.py
+++ django_prc/artists/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 class Artist(models.Model):
-    name = models.CharField(max_length=100)
-    dob = models.DateField()
-    gender = models.CharField(max_length=1)
+    GENDER_CHOICES =[
+        ('m', 'Male'),
+        ('f', 'female'),
+        ('o', 'others')
+    ]
+    name = models.CharField(max_length=255)
+    dob = models.DateTimeField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     address = models.CharField(max_length=255)
-    first_release_year 
+    first_release_year = models.CharField(max_length=4)
+    no_of_albums_released = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
