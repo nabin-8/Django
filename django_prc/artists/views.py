8b963404c857d434452e9103765cb8cb13aea642
nabin-8 <nabinacharya740@gmail.com>
2025-02-17 07:42:53 +0545

    Implement artist creation and listing functionality with templates

--- django_prc/artists/views.py
+++ django_prc/artists/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Artist
+from .forms import ArtistForm
+from django.shortcuts import redirect
 
-# Create your views here.
+def artist_list(request):
+    artists = Artist.objects.all()
+    return render(request, 'artists/artist_list.html', {'artists': artists})
+
+
+def artist_create(request):
+    if request.method == 'POST':
+        form = ArtistForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('artist_list')
+        else:
+            form = ArtistForm()
+        return render(request, 'artists/artist_form.html', {'form': form})
+
+
+def artist_detail(request, pk):
+    pass
