be7743039c470d2566f689732954f201d0a514d7
nabin-8 <nabinacharya740@gmail.com>
2025-02-21 21:58:18 +0545

    Enhance task management view by displaying tasks and completed tasks, and improve admin interface for Task model

--- django-to-do/todos/admin.py
+++ django-to-do/todos/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task', 'is_completed', 'updated_at')
+    search_fields = ('task',)
+    list_filter = ('is_completed',)
+
+admin.site.register(Task, TaskAdmin)
