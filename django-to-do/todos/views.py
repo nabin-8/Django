e702e8b3caf3173676ae2442916b3441610c3fcf
nabin-8 <nabinacharya740@gmail.com>
2025-02-22 03:49:08 +0545

    Add edit and delete functionality for tasks, including modal for editing

--- django-to-do/todos/views.py
+++ django-to-do/todos/views.py
@@ -15,3 +15,25 @@ def mark_as_done(request, pk):
     task.is_completed = True
     task.save()
     return redirect("home")
+
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect("home")
+
+
+def edit_task(request):
+    task_id = request.POST.get("task_id")
+    task_edit = request.POST.get("task_edit")
+    task = get_object_or_404(Task, pk=task_id)
+    task.task = task_edit
+    task.save()
+    return redirect("home")
+
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect("home")
